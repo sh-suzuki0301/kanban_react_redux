d87034fcfdf595dbfc14c0a9f1eff7b4e7c40e69
sh-suzuki0301 <sh-suzuki@oz-vision.co.jp>
2020-08-08 10:49:43 +0900

    基本のレイアウト作成

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,42 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 function App() {
-  return <h1>Hello React!!</h1>
+  return(
+    <div>
+      <header>
+        <div>Sho board</div>
+
+        <input placeholder="Filter"/>
+      </header>
+
+      <div>
+        <section>
+          <h3>TODO</h3>
+
+          <article>朝食</article>
+          <article>昼食</article>
+          <article>夕食</article>
+        </section>
+
+        <section>
+          <h3>Doing</h3>
+
+          <article>洗顔</article>
+          <article>歯磨き</article>
+        </section>
+
+        <section>
+          <h3>Wating</h3>
+        </section>
+
+        <section>
+          <h3>Done</h3>
+
+          <article>起きる</article>
+        </section>
+      </div>
+    </div>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById('app'))
