5083c0f91492a7c3e81df84f9a9ce285e8fa5685
sh-suzuki0301 <sh-suzuki@oz-vision.co.jp>
2020-08-10 15:59:10 +0900

    fix/comments

--- src/Card.tsx
+++ src/Card.tsx
@@ -7,7 +7,7 @@ export function Card({ text }: { text?: string }) {
   return (
     <Container>
       <CheckIcon />
-      {/* textにhttp/httpsが含まれてたらリンクにする */}
+      {/* textにhttp/https */}
       {text?.split(/(https?:\/\/\S+)/g).map((fragment, i) =>
         i % 2 === 0 ? (
           <Text key={i}>{fragment}</Text>
